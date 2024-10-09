9ed3462fbf54383d5ae329e1b1e5fb45c2fe60bc
Lee-Dongwook <dlehddnr0713@gmail.com>
2024-10-09 21:02:06 +0900

    chore : Dependency for project

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+// eslint-disable-next-line @typescript-eslint/no-require-imports
+const { createVanillaExtractPlugin } = require("@vanilla-extract/next-plugin");
+const withVanillaExtract = createVanillaExtractPlugin();
 
-export default nextConfig;
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: "/upload/:slug",
+        destination: `${process.env.NEXT_PUBLIC_BASE_URL}/:slug`,
+      },
+    ];
+  },
+};
+
+module.exports = withVanillaExtract(nextConfig);
