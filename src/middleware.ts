5e92fe08c96a46fb13464839e17669f48405ab77
Lee-Dongwook <dlehddnr0713@gmail.com>
2024-10-09 21:18:41 +0900

    auth.ts

---
+++ src/middleware.ts
@@ -0,0 +1,14 @@
+import { auth } from "./auth";
+import { NextResponse } from "next/server";
+
+export async function middleware() {
+  const session = await auth();
+
+  if (!session) {
+    return NextResponse.redirect("https://localhost:80/i/flow/login");
+  }
+}
+
+export const config = {
+  matcher: ["/compose/tweet", "/home", "/explore", "/messages", "/search"],
+};
